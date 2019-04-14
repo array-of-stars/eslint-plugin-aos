afac4399c5a0e2f13c7793644455c6217082bf2a
Ben Clarke <rod.parfait@gmail.com>
2019-04-14 10:53:16 -0400

    Initial commit

---
+++ rules/no-v-html-without-filter.js
@@ -0,0 +1,65 @@
+/**
+ * @fileoverview Rule to disallow use of v-html in vue templates without applying a filter
+ * @author Ben Clarke
+ */
+
+'use strict';
+
+const utils = require('eslint-plugin-vue/lib/utils');
+
+module.exports = {
+  meta: {
+    type: 'suggestion',
+    docs: {
+      description: 'disallow v-html without a filter',
+      category: 'Possible Errors',
+      recommended: true
+    },
+    fixable: 'code',
+    schema: [
+      {
+        'filter': {
+          'type': 'string'
+        }
+      }
+    ]
+  },
+  create(context) {
+
+    const template =
+      context.parserServices.getTemplateBodyTokenStore &&
+      context.parserServices.getTemplateBodyTokenStore();
+
+    const filter = context.options[0] || 'xss';
+
+    return utils.defineTemplateBodyVisitor(context, {
+      'VAttribute'(item) {
+
+        const key = item.key;
+        const val = item.value;
+
+        if ('html' !== key.name) {
+          return;
+        }
+
+        if (!val.expression) {
+          return;
+        }
+
+        if ('BinaryExpression' !== val.expression.type
+          || val.expression.operator !== '|'
+          || val.expression.right.name !== filter) {
+
+          const close = template.getLastToken(item);
+
+          context.report({
+            node: item,
+            message: '\'v-html\' directive does not filter its contents',
+            fix: (fixer) => fixer.insertTextBefore(close, ` | ${filter}`)
+          });
+        }
+
+      }
+    });
+  }
+};
